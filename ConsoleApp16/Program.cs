a5f1045c478a5e5b7d6d7f3c7345db7596cbcb4d
Sasha <alecsand.unit@gmail.com>
2024-11-17 14:39:19 +0200

    dz

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 class Good
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } 
     public double Price { get; set; }
     public string Category { get; set; }
 }
